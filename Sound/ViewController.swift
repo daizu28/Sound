1394759a7180f3fc784559f5a1979d54ed603c0f
Reina Kobayashi <kinokodaizukinako@gmail.com>
2020-09-15 07:41:42 +0900

    サウンドアプリ2の完成

--- Sound/ViewController.swift
+++ Sound/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var drumButton: UIButton!
 
     let drumSoundPlayer = try! AVAudioPlayer(data: NSDataAsset(name: "drumSound")!.data)
     
@@ -19,12 +21,19 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func tapDrumButton(){
+    @IBAction func touchDownDrumButton(){
+        
+        drumButton.setImage(UIImage(named: "drumPlayingImage"), for: .normal)
         
         drumSoundPlayer.currentTime = 0
         drumSoundPlayer.play()
         
         }
+    
+    @IBAction func touchDrumButton(){
+        
+        drumButton.setImage(UIImage(named: "drumImage"), for: .normal)
+    }
 
 
 }
